7f5d4ee22027eb156ada6e34f11cfa34d35b2ecb
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-05-24 15:48:18 +0000

    added test for GET /

--- tests/status.test.ts
+++ tests/status.test.ts
@@ -1,9 +1,25 @@
 import {createServer} from "../src/server";
 import Hapi from "@hapi/hapi";
-import {describe, expect, test} from "@jest/globals";
+import {describe, expect, test, beforeAll, afterAll} from "@jest/globals";
 
-describe("sum module", () => {
-  test("adds 1 + 2 to equal 3", () => {
-    expect(3).toBe(3);
+describe("Status plugin", () => {
+  let server: Hapi.Server;
+
+  beforeAll(async () => {
+    server = await createServer();
+  });
+
+  afterAll(async () => {
+    await server.stop();
+  });
+
+  test("status endpoint return 200", async () => {
+    const res = await server.inject({
+      method: "GET",
+      url: "/",
+    });
+    expect(res.statusCode).toEqual(200);
+    const response = JSON.parse(res.payload);
+    expect(response.up).toEqual(true);
   });
 });
